5371776a3744d26e481f6728022861c78b1cc1f2
Patrick Wu <Ptrckwu@yahoo.com>
2021-09-01 22:04:02 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(records){ //declaring superbowlWin function where the parameter records is an array
+    const found = records.find(function(entry){ //entry = element within records
+        return entry.result === "W" //returns the object with result: "W" into the found variable
+    }) 
+    if (found) { //if the .find didn't find anything, found will be undefined which is falsey
+        return found.year //this block will execute if records has a win
+    }
+}
