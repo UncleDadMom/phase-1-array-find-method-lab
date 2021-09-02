2b30d32dc8b19101d2a07f90dec5aa704c54dd33
Patrick Wu <Ptrckwu@yahoo.com>
2021-09-01 21:53:32 -0400

    commit tree ab1728e625d1f77bb653dfb371d2b918c49e2159

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(records){
+    const found = records.find(function(entry){
+        return entry.result === "W"
+    }) 
+    if (found) {
+        return found.year
+    }
+}
